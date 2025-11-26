05ac176cc353af2b016d3bce4bb0ee83646035f1
Daniel Emirah <danielemirah@gmail.com>
2025-11-26 22:18:45 +0300

    Création models Employee et test avec admin

--- crew/models.py
+++ crew/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Employee(models.Model):
+    name = models.CharField(max_length=200)
+    mail = models.EmailField()
+    phone = models.CharField(max_length=13)
+    poste = models.CharField(max_length=200)
+    salary = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.name}"
