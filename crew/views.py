ba0e601fdf5b8ad5194004f4d44b25da4bee1656
DanielEmirah <danielemirah@gmail.com>
2025-11-27 18:23:58 +0300

    Création listview2

--- crew/views.py
+++ crew/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from .models import Employee
 
 class EmployeeListView(ListView):
-    pass
+    model = Employee
